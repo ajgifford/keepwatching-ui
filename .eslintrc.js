e5eac1b4fc2cf9f79639b51cccd18fdade54d9de
Andy Gifford <ajgifford@users.noreply.github.com>
2025-11-02 22:12:30 -0600

    Initial setup of the project

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jest/recommended',
+    'prettier',
+  ],
+  rules: {
+    // General rules
+    'no-console': ['warn', { allow: ['error', 'warn', 'info'] }],
+    'no-unused-vars': 'off', // TypeScript has better unused var checking
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    '@typescript-eslint/ban-ts-comment': 'warn',
+
+    // React rules
+    'react/react-in-jsx-scope': 'off', // Not needed in React 17+
+    'react/prop-types': 'off', // TypeScript handles prop type checking
+    'react/display-name': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }],
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  singleQuote: true,
+  proseWrap: 'always',
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  importOrder: ['dotenv', 'react', '@mui', '<THIRD_PARTY_MODULES>'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
